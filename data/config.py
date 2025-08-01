66bc94c84d069118ba63567044a68a0e4659d138
Michael Lozitckii <worldspawn.web@gmail.com>
2025-08-01 13:15:54 +0300

    ref: changed the purpose of the bot (reset)

--- data/config.py
+++
@@ -1,5 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from core.market_data import BinanceMarketData
-from core.signal_engine import SignalEngine
-from tqdm import tqdm
-
-
 def main():
-    market = BinanceMarketData()
-    engine = SignalEngine(market)
-
-    print("[+] Starting pump short bot...")
-    symbols = market.get_active_symbols()
-
-    for symbol_data in tqdm(symbols, desc="Scanning symbols", ncols=100):
-        signal = engine.check_signal(symbol_data["symbol"])
-        if signal:
-            print(signal)
+    print("Starting the trading bot...")
 
 
 if __name__ == "__main__":
